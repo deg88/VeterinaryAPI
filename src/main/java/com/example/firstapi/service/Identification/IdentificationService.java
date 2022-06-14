65712f6741b337942288e29821926813e9da9ac6
degutierrezda <degutierrezda@poligran.edu.co>
2022-06-14 17:07:00 -0500

    se agrego las aprtes del CRUD a  la interface 'IdentificationService'.

--- src/main/java/com/example/firstapi/service/Identification/IdentificationService.java
+++ src/main/java/com/example/firstapi/service/Identification/IdentificationService.java
@@ -1,8 +1,22 @@
 package com.example.firstapi.service.Identification;
 
+import com.example.firstapi.dto.request.Identification.IdentificationDTOUpdate;
+import com.example.firstapi.dto.request.Identification.IdentificationDTOpost;
+import com.example.firstapi.dto.response.Identification.IdentificationDTO;
+
+import java.util.List;
+
 public interface IdentificationService {
 
+    IdentificationDTO getIdentificationById(int idIdentification);
 
+    List<IdentificationDTO> getAllIdentification();
+
+    IdentificationDTO createIdentification(IdentificationDTOpost tipoIdentificacionDTOPost);
+
+    IdentificationDTO updateTipoIdentificacion(IdentificationDTOUpdate identificacionDTOPost);
+
+    void deleteIdentificationById(int idIdentification);
 
 
 }
